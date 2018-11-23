6aa02f3c445d2b984f9d3c5862ef24b5ce68ab8b
chuticat <justine.delalleau@gmail.com>
2018-11-23 16:27:20 +0100

    convert table/ search/ button classes to functional stateless components

--- src/App.js
+++ src/App.js
@@ -23,48 +23,52 @@ const list = [
 const isSearched = searchTerm => item =>
 item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
-class Search extends Component {
-  render() {
-    const { value, onChange } = this.props;
+const Search = ({ value, onChange, children }) => {
     return (
       <form>
-        <input 
-          type="text"
-          value={ value }
-          onChange={ onChange }
+        { children } <input 
+          type = "text"
+          value = { value }
+          onChange = { onChange }
         />
       </form>
     );
   }
-}
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
+const Table = ({ list, pattern, onDismiss }) => {
     return (
       <div>
         {list.filter(isSearched(pattern)).map(item =>
-            <div key={item.objectID}>
+            <div key = { item.objectID }>
               <span>
-              <a href={item.url}>{item.title}</a>
+              <a href = { item.url }>{ item.title }</a>
               </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
+              <span>{ item.author }</span>
+              <span>{ item.num_comments }</span>
+              <span>{ item.points }</span>
               <span>
-                <button
-                  onClick={ () => onDismiss(item.objectID) }
-                  type="button"
-                >
+                <Button onClick={ () => onDismiss(item.objectID) }>
                   Dismiss
-                </button>
+                </Button>
               </span>
             </div>
           )}
       </div>
     );
   }
-}
+
+
+const Button = ({onClick, className = '', children}) => {
+    return (
+      <button
+        onClick = { onClick }
+        className = { className }
+        type = "button"
+      >
+        { children }
+      </button>
+    )
+  }
 
 class App extends Component {
 
@@ -94,13 +98,15 @@ class App extends Component {
     return (
       <div className="App">
         <Search 
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        />
+          value = { searchTerm }
+          onChange = { this.onSearchChange }
+        >
+          Search
+        </Search>
         <Table 
-          list={list}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
+          list = { list }
+          pattern = { searchTerm }
+          onDismiss = { this.onDismiss }
         />
       </div>
     );
