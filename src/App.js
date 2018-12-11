6842984d8cd185093db65ac4d475308805c7fdd7
chuticat <justine.delalleau@gmail.com>
2018-12-11 09:59:27 +0100

    add error handler

--- src/App.js
+++ src/App.js
@@ -82,6 +82,7 @@ class App extends Component {
       results : null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     };
 
     this.onDismiss = this.onDismiss.bind(this);
@@ -118,7 +119,7 @@ class App extends Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
+      .catch(error => this.setState({ error }));
   }
 
   componentDidMount() {
@@ -161,7 +162,8 @@ class App extends Component {
     const { 
       searchTerm, 
       results, 
-      searchKey 
+      searchKey,
+      error,
     } = this.state;
 
     const page = (
@@ -187,10 +189,17 @@ class App extends Component {
             Search
           </Search>
         </div>
+        {
+          error
+          ? <div className="interactions">
+            <p>Something went wrong.</p>
+          </div>
+          :
           <Table 
             list = { list }             
             onDismiss = { this.onDismiss }
           />
+        }
         <div className="interactions">
           <Button onClick={ () => this.fetchSearchTopStories(searchKey, page + 1)}>
             More
